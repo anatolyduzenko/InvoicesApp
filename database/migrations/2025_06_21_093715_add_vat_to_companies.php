eb5c951c1939098a4c3008f4b699a709ea34db29
Anatoly Duzenko <anatoliy.duzenko@gmail.com>
2025-06-21 07:23:54 -0400

    Migrations for new columns

---
+++ database/migrations/2025_06_21_093715_add_vat_to_companies.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('vat_applicable', 50)->after('company_phone')->default('');
+            $table->string('vat_number', 50)->after('vat_applicable')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropColumn(['vat_applicable', 'vat_number']);
+        });
+    }
+};
